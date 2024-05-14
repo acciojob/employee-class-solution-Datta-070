b01b376f91665012c7f9883f4167d6098242b87d
Chinmay Datt D <chinmaydatt003@gmail.com>
2024-05-14 11:03:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,30 @@
-function Employee(name, salary) {
-  this.name = name;
-  this.salary = salary;
+class Employee {
+    constructor(name, salary) {
+        this.name = name;
+        this.salary = salary;
+    }
+
+    giveRaise(amount) {
+        this.salary += amount;
+    }
 }
 
-Employee.prototype.giveRaise = function(amount) {
-  this.salary += amount;
+class Manager extends Employee {
+    constructor(name, salary, department) {
+        super(name, salary);
+        this.department = department;
+    }
+
+    giveRaise(amount) {
+        super.giveRaise(amount);
+        console.log(`New salary for ${this.name} in ${this.department}: ${this.salary}`);
+    }
 }
 
-function Manager(name, salary, department) {
-  Employee.call(this, name, salary);
-  this.department = department;
-}
+// Sample Input
+let employee = new Employee("John Doe", 50000);
+employee.giveRaise(5000);
+console.log(employee.salary); // Output: 55000
 
-Manager.prototype = Object.create(Employee.prototype);
-Manager.prototype.constructor = Manager;
-
-Manager.prototype.giveRaise = function(amount) {
-  Employee.prototype.giveRaise.call(this, amount);
-  console.log(`New salary for ${this.name} in ${this.department}: ${this.salary}`);
-}
+let manager = new Manager("Jane Smith", 80000, "Sales");
+manager.giveRaise(10000); // Output: New salary for Jane Smith in Sales: 90000
